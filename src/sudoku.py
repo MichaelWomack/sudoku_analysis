2e35a783ebb121c7bcf902428189a9ad8ef8d671
Michael Womack <michaelwomack@Michaels-MacBook-Pro.local>
2016-01-17 17:38:08 -0500

    init function checkes col, row, and nonet for unique --needs work

--- src/sudoku.py
+++ src/sudoku.py
@@ -6,13 +6,17 @@
 # Started out creating 2d arrays for rows, cols, nonets.
 # Step one: Implement unique placement in row and column
 
+# First Run with Row, Col, Nonet Rule in place: 42 wrong out of 81, not all zeros taken care of
 
 class Sudoku:
     'Create an instance of Sudoku to solve'
 
     def __init__(self, file):
+        self.game_string = open(file, 'r').read()
         self.all_boxes = open(file, 'r').read()
-        self.solution = open('../solution.txt', 'r').read()
+        self.solution = [list(x) for x in open('../solution.txt', 'r').read().split()]
+        # self.rows = [list(row) for row in self.game_string.split()]
+        # self.cols = [x[index] for x in self.rows for index in range(9)]
         self.rows = []
         self.cols = []
         self.nonets = []
@@ -30,7 +34,8 @@ class Sudoku:
         for index in range(9):
             self.cols.append([x[index] for x in self.rows])
 
-    # Stores nonets as lists
+
+    # Fills list of nonets from game's file
     def get_nonets(self):
         index = 0
         row_start = 0
@@ -70,38 +75,49 @@ class Sudoku:
 
     # Checks if solution is correct
     def check_solution(self):
-        return self.all_boxes == open('../solution.txt', 'r').read()
+        return self.rows == self.solution
 
     # Counts number of wrong boxes by comparing with solution
     def get_number_wrong(self):
-        solution = list(open('../solution.txt', 'r').read())
         num_wrong = 0
-        for box in list(self.all_boxes):
-            if box != solution[self.all_boxes.index(box)]:
-                num_wrong += 1
+        for row in self.rows:
+            row_index = self.rows.index(row)
+            for box in row:
+                box_index = row.index(box)
+                if box != self.solution[row_index][box_index]:
+                    num_wrong += 1
         return num_wrong
 
 
     # Initialize game
-    # def init_game(self):
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #     print("\nMy answer: \n{}".format(self.all_boxes))
-    #     print("\nCorrect Solution: \n{}".format(self.solution))
-    #     print("\nNumber of Wrong Placements: {}".format(self.get_number_wrong()))
+    def init_game(self):
+        for number in range(1, 10):
+            for nonet in self.nonets:
+                nonet_index = self.nonets.index(nonet)
+                if str(number) not in nonet:
+                    for box in nonet:
+                        box_index = nonet.index(box)
+                        if box == '0':
+                            row_to_check = int(nonet_index / 3) * 3 + int(box_index / 3)
+                            col_to_check = int(nonet_index % 3) * 3 + int(box_index % 3)
+                            if self.valid_in_row(str(number), row_to_check) and self.valid_in_col(str(number), col_to_check):
+                                    nonet[box_index] = str(number)
+                                    self.cols[col_to_check][row_to_check] = str(number)
+                                    self.rows[row_to_check][col_to_check] = str(number)
+            if self.is_complete():
+                self.game_complete = True
+        print("My Answer: {}\n".format(self.rows))
+        print("Solution: {}\n".format(self.solution))
+        print("Number wrong: {}".format(self.get_number_wrong()))
+
+
+
+
+
+
 
 s = Sudoku('../game.txt')
-print(s.nonets)
-#print(s.all_boxes.split('\n'))
-# print(s.all_boxes)
-# print(s.valid_in_row('3', 0))
-# print("is complete: {}".format(s.is_complete()))
-# s.init_game()
+
+# s.all_boxes = [list(x) for x in s.all_boxes.split()] # rows
+s.init_game()
+
